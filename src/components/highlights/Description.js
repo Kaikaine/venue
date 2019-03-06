d58fd85ff30066e2d6b9e5bf65121d219c634958
kaikaine <kairijb@protonmail.com>
2019-03-06 10:25:13 -0600

    added highlight section

---
+++ src/components/highlights/Description.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade'
+const Description = () => {
+
+    return ( 
+    <div>
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className='highlight_description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum consectetur ex a lacinia. Curabitur dapibus mauris tellus, vitae tempus neque lobortis non. Vestibulum quis auctor elit.
+                </div>
+            </div>
+        </Fade>
+    </div> );
+}
+ 
+export default Description;
---
+++ src/components/highlights/Discount.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade'
+import Slide from 'react-reveal/Slide'
+
+class Discount extends Component {
+    state = { 
+        discountStart: 0,
+        discountEnd: 30
+     }
+
+    percentage = () => {
+        if(this.state.discountStart < this.state.discountEnd) {
+            this.setState({discountStart: this.state.discountStart +1})
+        } 
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        setTimeout(() => {
+            this.percentage()
+        }, 30)
+    }
+    
+    render() { 
+        return ( 
+        <div className='center_wrapper'>
+            <div className="discount_wrapper">
+
+            <Fade
+            onReveal={() => this.percentage()}
+            >
+            <div className='discount_porcentage'>
+                    <span>{this.state.discountStart}%</span>
+                    <span>OFF</span>
+                </div>
+            </Fade>
+                
+            <Slide right>
+                <div className='discount_description'>
+                    <h3>Purchase tickets before FEB 2nd</h3>
+                    <p>Duis quis felis non turpis porttitor rhoncus. Nunc non dapibus risus. Ut risus dui, pellentesque at nunc vitae, porta posuere leo. Aliquam ut felis non nulla pulvinar lobortis id sit amet massa. Integer nec ipsum placerat, ullamcorper odio nec, vulputate risus.</p>
+                    <div>
+                        button
+                    </div>
+                </div>
+                
+            </Slide>
+                
+
+                
+            </div>
+        </div> );
+    }
+}
+ 
+export default Discount;

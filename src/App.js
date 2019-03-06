25b3980eb9e6f180e4e2bff22b9aba2df7af0d1a
kaikaine <kairijb@protonmail.com>
2019-03-06 12:20:00 -0600

    side drawer scrolls to sections now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import './resources/styles.css'
-
-import Header from './components/header_footer/Header';
-import Featured from './components/featured';
-import VenueInfo from './components/venue_info';
-import Highlights from './components/highlights';
-import Pricing from './components/pricing';
-import Location from './components/location/index';
-import Footer from './components/header_footer/Footer';
-
+import React, { Component } from "react";
+import "./resources/styles.css";
+import { Element } from "react-scroll";
+import Header from "./components/header_footer/Header";
+import Featured from "./components/featured";
+import VenueInfo from "./components/venue_info";
+import Highlights from "./components/highlights";
+import Pricing from "./components/pricing";
+import Location from "./components/location/index";
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height: '1500px'}}>
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
-        <Featured />
-        <VenueInfo />
-        <Highlights />
-        <Pricing />
-        <Location />
+        <Element name="event">
+          <Featured />
+        </Element>
+        <Element name="venue">
+          <VenueInfo />
+        </Element>
+        <Element name="highlight">
+          <Highlights />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
+
         <Footer />
       </div>
     );
--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -2,8 +2,20 @@ import React from "react";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import {scroller} from 'react-scroll';
 
 const SideDrawer = (props) => {
+
+  const scrollTo = (el) => {
+    scroller.scrollTo(el, {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: -150
+    })
+    props.onClose(false)
+  }
+
   return (
     <div>
       <Drawer
@@ -12,11 +24,11 @@ const SideDrawer = (props) => {
       onClose={() => props.onClose(false)}
       >
         <List component='nav'>
-            <ListItem button onClick={() => console.log()}>Event Starts In</ListItem>
-            <ListItem button onClick={() => console.log()}>Venue Info</ListItem>
-            <ListItem button onClick={() => console.log()}>Highlights</ListItem>
-            <ListItem button onClick={() => console.log()}>Pricing</ListItem>
-            <ListItem button onClick={() => console.log()}>Location</ListItem>
+            <ListItem button onClick={() => scrollTo('event')}>Event Starts In</ListItem>
+            <ListItem button onClick={() => scrollTo('venue')}>Venue Info</ListItem>
+            <ListItem button onClick={() => scrollTo('highlight')}>Highlights</ListItem>
+            <ListItem button onClick={() => scrollTo('pricing')}>Pricing</ListItem>
+            <ListItem button onClick={() => scrollTo('location')}>Location</ListItem>
         </List>
       </Drawer>
     </div>
